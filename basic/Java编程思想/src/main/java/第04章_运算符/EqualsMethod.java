bfacae0d4047a30e6be2b354f1b46e8e80ed62ba
19920625lsg <1648266192@qq.com>
2020-03-04 01:04:52 +0800

    第4章开始啦

---
+++ basic/Java编程思想/src/main/java/第04章_运算符/EqualsMethod.java
@@ -0,0 +1,16 @@
+/***********************************************************
+ * @Description : 对象相等
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2020/3/4 1:04
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package 第04章_运算符;
+
+// operators/EqualsMethod.java
+public class EqualsMethod {
+    public static void main(String[] args) {
+        Integer n1 = 47;
+        Integer n2 = 47;
+        System.out.println(n1.equals(n2));
+    }
+}
